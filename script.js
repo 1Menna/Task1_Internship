33e4d58f45ce8da54a74dae1d387f15d1998f76a
MaD-Steven <stevennagy788@gmail.com>
2025-07-25 03:24:17 +0300

    Adding the edit functionality and giving the table data id's

--- script.js
+++ script.js
@@ -24,12 +24,41 @@ function display(){
     for(var i=0; i<list.length ; i++){
         cartona += 
         `   <tr> 
-                <td>${list[i].task}</td>
-                <td>${list[i].des}</td>
-                <td><button>Delete</button></td>
+                <td id="${'title'+i}">${list[i].task}</td>
+                <td id="${'des'+i}">${list[i].des}</td>
+                <td id="${'edit'+i}"><button onclick="editTask(${i})">Edit</button></td>
+                <td id="${'del'+i}"><button>Delete</button></td>
             </tr>
         `
     }
     var L = document.getElementById("taskList");
     L.innerHTML = cartona;
+}
+
+function confirmUpdate (id) {
+    // list[id].task = document.getElementById("titleUpdate").value;
+    // list[id].des= document.getElementById("desUpdate").value;
+    // console.log(list); for checking
+    const titleUp = document.getElementById("titleUpdate");
+    const desUp = document.getElementById("desUpdate");
+    const updatedTasks = list.map((task, i) => {
+        if (i === id) {
+            return { task: titleUp.value, des: desUp.value };
+        }
+        return task;
+    });
+    list = updatedTasks;
+    // console.log(updatedTasks);
+    display();
+}
+function editTask(id){
+    const titleUp = document.getElementById('title'+id);
+    const desUp = document.getElementById('des'+id);
+    const confirmBtn = document.getElementById('edit'+id);
+    const cancelBtn = document.getElementById('del'+id);
+
+    titleUp.innerHTML = `<input type="text" id="titleUpdate" value="${list[id].task}">`;
+    desUp.innerHTML = `<input type="text" id="desUpdate" value="${list[id].des}">`;
+    confirmBtn.innerHTML = `<button onclick="confirmUpdate(${id})">Confirm</button>`;
+    cancelBtn.innerHTML = `<button onclick="display()">Cancel</button>`;
 }
